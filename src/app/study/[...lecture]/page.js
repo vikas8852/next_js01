3b1c8ba2ee091a31b6868e7f64aad9a02ac289c4
Vikas Verma <vikasverma3552@gmail.com>
2024-12-27 22:41:00 +0530

    catch all

---
+++ src/app/study/[...lecture]/page.js
@@ -0,0 +1,10 @@
+" use client"
+export default function Lecture(params){
+  
+    return(
+        <div>
+            <h1> Lectures of  1</h1>
+            <h2>{params.lecture}</h2>
+        </div>
+    )
+}
---
+++ src/app/study/page.js
@@ -0,0 +1,7 @@
+export default function Study(){
+    return(
+        <div>
+            <h1>All Lectures of college</h1>
+        </div>
+    )
+}
