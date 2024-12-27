874120708a5850d799139c398a0a4a279514489c
Vikas Verma <vikasverma3552@gmail.com>
2024-12-27 21:43:59 +0530

    conditonal layout

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,23 +1,30 @@
+"use client"
 import Link from 'next/link'
 import './login.css'
-export default function Layout({
-    children
+import {usePathname} from "next/navigation"
 
-}){
+export default function Layout({ children}){
+    const pathName=usePathname();
+    console.log(pathName)
     return(
     <div>
+    {
+        pathName!=="/login/loginteacher"?
         <ul className="login-menu">
-            <li> <h4>Navbar</h4></li>
-            <li  >
-                <Link href="/login">Login Main </Link>
-            </li>
-            <li>
-                <Link href="/login/loginstudent"> Students login </Link>
-            </li>
-            <li>
-                <Link href="/login/loginteacher"> Teacher Login</Link>
-            </li>
-        </ul>
+        <li> <h4>Navbar</h4></li>
+        <li  >
+            <Link href="/login">Login Main </Link>
+        </li>
+        <li>
+            <Link href="/login/loginstudent"> Students login </Link>
+        </li>
+        <li>
+            <Link href="/login/loginteacher"> Teacher Login</Link>
+        </li>
+    </ul>
+    :<Link href="/login">Go to Main Login</Link>
+    }
+    
         {
             children
         }
